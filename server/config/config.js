08427d7cdcda339b879c1a29948dc38f52f55131
Rithvik Vasishta <rithvikvasishta@gmail.com>
2020-05-11 03:49:03 +0530

    configured mongoose

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 const envVars=process.env
+
 module.exports={
     port: envVars.PORT,
-    env: envVars.NODE_ENV
+    env: envVars.NODE_ENV,
+    mongo: {
+        uri:envVars.MONGDB_URI,
+        port:envVars.MONGO_PORT,
+        isDebug:envVars.MONGOOSE_DEBUG
+    }
 };
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const app = require('./config/express');
 const config = require('./config/config');
 
+//initiate db module
+require('./config/mongoose');
 
 app.listen(config.port,()=>{
     console.log(`server started on port ${config.port} (${config.env})`);
