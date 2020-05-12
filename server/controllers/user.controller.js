20e2dea32e2f1f3400df246f17fa5957bbed24d1
Rithvik Vasishta <rithvikvasishta@gmail.com>
2020-05-12 10:22:15 +0530

    saving user to mongodb

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,9 +1,14 @@
-users = [];
+const User = require('../models/user.model');
+const bcrypt = require('bcrypt')
 async function insert(user){
+    
+    user.hashedPassword = bcrypt.hashSync(user.password,10);
+    delete user.password;
     //make mongo call to save user in db
     console.log(`saving user to db`,user);
-    users.push(user);
-    return user
+
+    return await new User(user).save();
+    
 }
 
 module.exports = {
