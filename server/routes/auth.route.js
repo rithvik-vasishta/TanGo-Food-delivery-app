68870b04e84d6c926e1d2eb4f1551330d6d6e52b
Rithvik Vasishta <rithvikvasishta@gmail.com>
2020-05-13 20:49:38 +0530

    added middleware to routes

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -2,25 +2,37 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const userController = require('../controllers/user.controller');
 const asyncHandler = require('express-async-handler');
+const authController = require('../controllers/auth.controller');
 
 const router = express.Router();
-router.post('/register',asyncHandler(insert));
-router.post('/login',asyncHandler(getUserByEmailIdAndPassword));
+
+
+//localhost:4050/api/auth/register
+router.post('/register',asyncHandler(insert), login);
+router.post('/login',asyncHandler(getUserByEmailIdAndPassword), login);
 
 async function insert(req,res,next){
     const user = req.body;
     console.log('regestering the user', user);
-    const savedUser =  await userController.insert(user);
-    res.json(savedUser);
+    req.user =  await userController.insert(user);
+    next();
 }
 
 async function getUserByEmailIdAndPassword(req, res, next){
     const user = req.body;
     console.log(`searching user for `, user);
 
-    const savedUser = await userController.getUserByEmailIdAndPassword(user.email,user.password);
+    req.user = await userController.getUserByEmailIdAndPassword(user.email,user.password);
 
-    res.json(savedUser);
+    next();
+}
+
+function login(req, res) {
+    const user = req.user;
+    const token = authController.generateToken(user);
+    res.json({
+        user,token
+    });
 }
 
 
