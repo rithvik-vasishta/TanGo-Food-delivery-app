6ced250f06fdfcfb56a53e062c0becfec40d0b0d
Rithvik Vasishta <rithvikvasishta@gmail.com>
2020-05-16 21:09:48 +0530

    created blocks module, header comp(inside blocks)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './blocks/root/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthHeaderInterceptorService } from './interceptors/auth-header-interceptor.service';
 import { SharedModule } from './shared/shared.module';
+import { BlocksModule } from './blocks/blocks.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
     HomeComponent
   ],
   imports: [
@@ -19,7 +19,8 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
-    HttpClientModule   
+    HttpClientModule,
+    BlocksModule   
     
   ],
   providers: [
