c3b5fdc7c7b7d3d19ee9c0bb48d3c3c336e445c5
Rithvik Vasishta <rithvikvasishta@gmail.com>
2020-05-16 23:53:30 +0530

    debugged error in core.module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -24,7 +24,7 @@ import { SharedModule } from '../shared/shared.module';
   ]
 })
 export class CoreModule {
-  constructor(@Optional() @SkipSelf() parentModule){
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule){
     throwIfAlreadyLoaded(parentModule,'CoreModule');
   }
  }
