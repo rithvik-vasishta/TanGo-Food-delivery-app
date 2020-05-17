39257f6f6d03845abb144afc14dab98d63264af1
Rithvik Vasishta <rithvikvasishta@gmail.com>
2020-05-18 03:55:33 +0530

    added update, delete and restore cart

--- src/app/core/cart/cart-store.spec.ts
+++ src/app/core/cart/cart-store.spec.ts
@@ -1,6 +1,6 @@
 import { CartStore } from './cart-store';
 import { TestBed } from '@angular/core/testing';
-import { initialState } from './cart-state';
+import { initialState, CartState } from './cart-state';
 import { CartItem } from './cart-item';
 
 describe('CartStore', () => {
@@ -40,4 +40,139 @@ describe('CartStore', () => {
 
     expect(cartStore.state).toEqual(expectedState);
   });
+
+
+  it('can clear cart ',()=>{
+      const cartItem: CartItem = {
+      productId:1,
+      imgUrl:'img/apple',
+      price:2,
+      quantity:10,
+      itemTotal:20,
+      name:'apple'
+    };
+
+    cartStore.addCartItem(cartItem)
+
+    const currentState ={
+      cartItems:[cartItem]
+    };
+
+    expect(cartStore.state).toEqual(currentState);
+
+
+    cartStore.clearCart();
+
+
+    expect(cartStore.state).toEqual(initialState);
+
+  });
+
+
+  it('can restore cart ',()=>{
+
+    const currentState=initialState;  
+
+    expect(cartStore.state).toEqual(currentState);
+    const cartItem: CartItem = {
+      productId:1,
+      imgUrl:'img/apple',
+      price:2,
+      quantity:10,
+      itemTotal:20,
+      name:'apple'
+    };
+    const expectedState: CartState = {
+      cartItems: [cartItem]
+    };
+
+    cartStore.restoreCart(expectedState);
+
+    expect(cartStore.state).toEqual(expectedState);
+
+});
+
+
+it('can remove cart ',()=>{
+
+  const cartItem: CartItem = {
+    productId:2,
+    imgUrl:'img/apple',
+    price:2,
+    quantity:10,
+    itemTotal:20,
+    name:'apple'
+  };
+
+  const cartItem1: CartItem = {
+    productId:1,
+    imgUrl:'img/orange',
+    price:20,
+    quantity:100,
+    itemTotal:200,
+    name:'orange'
+  };
+  const currentState: CartState = {
+    cartItems: [cartItem, cartItem1]
+  };
+
+  cartStore.restoreCart(currentState);
+
+  expect(cartStore.state).toEqual(currentState);
+
+
+  cartStore.removeCartItem(cartItem);
+
+  const expectedState: CartState = {
+    cartItems: [cartItem1]
+  }
+
+  expect(cartStore.state).toEqual(expectedState);
+
+});
+
+it('can update cart ',()=>{
+
+  const cartItem: CartItem = {
+    productId:1,
+    imgUrl:'img/apple',
+    price:2,
+    quantity:10,
+    itemTotal:20,
+    name:'apple'
+  };
+
+  const cartItem1: CartItem = {
+    productId:2,
+    imgUrl:'img/orange',
+    price:20,
+    quantity:100,
+    itemTotal:200,
+    name:'orange'
+  };
+  const currentState: CartState = {
+    cartItems: [cartItem, cartItem1]
+  };
+
+  cartStore.restoreCart(currentState);
+
+  expect(cartStore.state).toEqual(currentState);
+
+  const cartItemToUpdate: CartItem = {
+    productId:2,
+    imgUrl:'img/orange',
+    price:20,
+    quantity:110,
+    itemTotal:330,
+    name:'orange'
+  };
+  cartStore.updateCartItem(cartItemToUpdate);
+
+  const expectedState: CartState = {
+    cartItems: [cartItem,cartItemToUpdate]
+  }
+
+  expect(cartStore.state).toEqual(expectedState);
+
+});
 });
--- src/app/core/cart/cart-store.ts
+++ src/app/core/cart/cart-store.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ɵɵstaticContentQuery } from '@angular/core';
 import { Store } from '@core/store';
 import { CartState, initialState } from './cart-state';
 
 @Injectable({ providedIn: 'root'})
 export class CartStore extends Store<CartState> {
-    
+  
     constructor(){
         super(initialState);
     }
@@ -19,4 +19,41 @@ export class CartStore extends Store<CartState> {
 
         this.setState(newState);
     }
+
+    clearCart() {
+        console.log('[cart] clear cartItem');
+        const newState = initialState;
+
+        this.setState(newState);
+      }
+
+      restoreCart(stateToRestore: CartState) {
+        console.log('[cart] restore cartItem');
+        this.setState(stateToRestore)
+      }
+
+      removeCartItem(cartItemToRemove: import("./cart-item").CartItem) {
+        console.log('[cart] remove cartItem');
+        const newState = {
+            ...this.state,   //cartItems
+            cartItems: this.state.cartItems.filter(cartItem=> cartItem.productId != cartItemToRemove.productId)
+            
+        };
+
+        this.setState(newState);
+      }  
+
+      updateCartItem(cartItemToUpdate: import("./cart-item").CartItem) {
+        console.log('[cart] update cartItem');
+        const newState = {
+            ...this.state,   //cartItems
+            cartItems: this.state.cartItems.map(cartItem=> 
+                cartItem.productId == cartItemToUpdate.productId 
+                ? cartItemToUpdate
+                : cartItem)
+            
+        };
+
+        this.setState(newState);
+      }
 }
