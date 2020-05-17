a6dc11195553143b94a9c628dfc03c7fde59a4b0
Rithvik Vasishta <rithvikvasishta@gmail.com>
2020-05-18 00:34:49 +0530

    added filter and pagination

--- src/app/products/products/products.component.ts
+++ src/app/products/products/products.component.ts
@@ -3,6 +3,7 @@ import { ProductDataService } from '@core/products/product-data.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { Product } from '@core/products/product';
 import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'sw-products',
@@ -16,6 +17,8 @@ export class ProductsComponent implements OnInit, OnDestroy {
   displayedColumns = ['imgUrl', 'name', 'price', 'action'];
   
   @ViewChild(MatSort) sort:MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
 
   constructor(private productDataService: ProductDataService) { }
   ngOnDestroy(): void {
@@ -29,9 +32,16 @@ export class ProductsComponent implements OnInit, OnDestroy {
       .subscribe(products => this.onDataLoad(products)))
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+
   onDataLoad(products){
     this.loading = false;
     this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
     this.dataSource.data = products;
     
   }
