80ae4411b00d07f259811667b458629002f4dc39
Rithvik Vasishta <rithvikvasishta@gmail.com>
2020-05-17 23:48:38 +0530

    made products page responsive

--- src/app/products/products/products.component.ts
+++ src/app/products/products/products.component.ts
@@ -1,19 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { ProductDataService } from '@core/products/product-data.service';
-import { Observable } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+import { Product } from '@core/products/product';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'sw-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent implements OnInit {
-  products: Observable<any>;
+export class ProductsComponent implements OnInit, OnDestroy {
+  dataSource = new MatTableDataSource<Product>();
+  loading: boolean= true;
+  subscriptions =[];
+  displayedColumns = ['imgUrl', 'name', 'price', 'action'];
+  
+  @ViewChild(MatSort) sort:MatSort;
 
-  constructor(private ProductDataService: ProductDataService) { }
+  constructor(private productDataService: ProductDataService) { }
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(s => s.unsubscribe());
+  }
 
   ngOnInit(): void {
-    this.products = this.ProductDataService.getAllProducts();
+    this.subscriptions.push
+    (this.productDataService
+      .getAllProducts()
+      .subscribe(products => this.onDataLoad(products)))
+  }
+
+  onDataLoad(products){
+    this.loading = false;
+    this.dataSource.sort = this.sort;
+    this.dataSource.data = products;
+    
   }
 
 }
