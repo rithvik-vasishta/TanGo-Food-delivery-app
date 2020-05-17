e44c9f40c22dd264f8fa84c43cffb0c673c529e6
Rithvik Vasishta <rithvikvasishta@gmail.com>
2020-05-17 19:05:59 +0530

    OnPush strategy

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/auth/auth.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'sw-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent implements OnInit {
   public get router(): Router {
@@ -16,19 +18,24 @@ export class LoginComponent implements OnInit {
   }
   email:string;
   password:string;
-  error:string;
+  error:BehaviorSubject<string>;
 
   constructor(private _router: Router, private authService:AuthService) { }
 
   ngOnInit(): void {
+    this.error = new BehaviorSubject('');
   }
   login(){
-    this.error = '';
+    this.setError('');
     this.authService
     .login(this.email,this.password)
-    .subscribe(s=>this.router.navigate(['']),e => (this.error = e)
+    .subscribe(s=>this.router.navigate(['']),e => (this.setError(e))
     );
 
   }
 
+
+  private setError(msg : any) {
+    return this.error.next(msg);
+  }
 }
