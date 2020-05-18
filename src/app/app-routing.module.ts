8fba139a824089d7d67d4b107282b3a077ee11dc
Rithvik Vasishta <rithvikvasishta@gmail.com>
2020-05-18 19:10:48 +0530

    created cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,10 @@ const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.module').then(m => m.CartModule)
   }
   
 ];
