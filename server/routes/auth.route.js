1f7afbc15461b7fdaf10f62776c976bf8d1b1251
Rithvik Vasishta <rithvikvasishta@gmail.com>
2020-05-21 10:21:30 +0530

    fixed login issue

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,39 +1,45 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const userController = require('../controllers/user.controller');
-const asyncHandler = require('express-async-handler');
-const authController = require('../controllers/auth.controller');
-const passport = require('../middleware/passport');
+const express = require("express");
+
+const asyncHandler = require("express-async-handler");
+const userController = require("../controllers/user.controller");
+const authController = require("../controllers/auth.controller");
+const passport = require("../middleware/passport");
+
 const router = express.Router();
 
+// localhost:4050/api/auth/register
+router.post("/register", asyncHandler(insert), login);
+router.post("/login", asyncHandler(getUserByEmailIdAndPassword), login);
+router.get("/findme", passport.authenticate("jwt", { session: false }), login);
 
-//localhost:4050/api/auth/register
-router.post('/register',asyncHandler(insert), login);
-router.post('/login',passport.authenticate("local", {session : false}), login);
-router.get('/findme', passport.authenticate("jwt",{ session : false }), login);
-
-async function insert(req,res,next){
-    const user = req.body;
-    console.log('regestering the user', user);
-    req.user =  await userController.insert(user);
-    next();
+async function insert(req, res, next) {
+  const user = req.body;
+  console.log(`registering user`, user);
+  req.user = await userController.insert(user);
+  next();
 }
 
-async function getUserByEmailIdAndPassword(req, res, next){
-    const user = req.body;
-    console.log(`searching user for `, user);
-    req.user = await userController.getUserByEmailIdAndPassword(user.email,user.password);
-    next();
-}
+async function getUserByEmailIdAndPassword(req, res, next) {
+  const user = req.body;
+  console.log(`searching user for `, user);
 
+  const savedUser = await userController.getUserByEmailIdAndPassword(
+    user.email,
+    user.password
+  );
+
+  req.user = savedUser;
+
+  next();
+}
 
 function login(req, res) {
-    const user = req.user;
-    const token = authController.generateToken(user);
-    res.json({
-        user,token
-    });
+  const user = req.user;
+  const token = authController.generateToken(user);
+  res.json({
+    user,
+    token
+  });
 }
-
 
 module.exports = router;
